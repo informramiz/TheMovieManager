e955faa8da6d8e351fe323153f5dd6fa24f62f64
Ramiz Raja <informramiz@gmail.com>
2019-07-17 04:10:37 +0500

    feat: Write model for session API response

--- TheMovieManager/Model/TMDB Responses/SessionResponse.swift
+++ TheMovieManager/Model/TMDB Responses/SessionResponse.swift
@@ -3,9 +3,18 @@
 //  TheMovieManager
 //
 //  Created by Owen LaRosa on 8/13/18.
+//  Updated by Ramiz Raja on 14/07/19
 //  Copyright © 2018 Udacity. All rights reserved.
 //
 
 import Foundation
 
-
+struct SessionResponse: Codable {
+    let success: Bool
+    let sessionId: String
+    
+    enum CodingKeys: String, CodingKey {
+        case success
+        case sessionId = "session_id"
+    }
+}
