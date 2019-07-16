e0d0205ec2766ca39282dc560b28090a0190f90a
Ramiz Raja <informramiz@gmail.com>
2019-07-17 04:11:06 +0500

    feat: Write model for create session request

--- TheMovieManager/Model/TMDB Requests/PostSession.swift
+++ TheMovieManager/Model/TMDB Requests/PostSession.swift
@@ -3,9 +3,16 @@
 //  TheMovieManager
 //
 //  Created by Owen LaRosa on 8/13/18.
+//  Updated by Ramiz Raja on 14/07/19
 //  Copyright © 2018 Udacity. All rights reserved.
 //
 
 import Foundation
 
-
+struct PostSession: Codable {
+    let requestToken: String
+    
+    enum CodingKeys: String, CodingKey {
+        case requestToken = "request_token"
+    }
+}
--- TheMovieManager/Model/TMDB Responses/RequestTokenResponse.swift
+++ TheMovieManager/Model/TMDB Responses/RequestTokenResponse.swift
@@ -3,6 +3,7 @@
 //  TheMovieManager
 //
 //  Created by Owen LaRosa on 8/13/18.
+//  Updated by Ramiz Raja on 14/07/19
 //  Copyright © 2018 Udacity. All rights reserved.
 //
 
