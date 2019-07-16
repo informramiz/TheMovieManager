2a54d2b8fb7ecc8b1ba5d1199231fa00b8041cbf
Ramiz Raja <informramiz@gmail.com>
2019-07-16 22:56:51 +0500

    feat: Add request new token endpoint

--- TheMovieManager/Model/TMDB Client/TMDBClient.swift
+++ TheMovieManager/Model/TMDB Client/TMDBClient.swift
@@ -10,6 +10,7 @@ import Foundation
 
 class TMDBClient {
     
+    //TODO: Remove it and move it to somewhere safe
     static let apiKey = "8a7b4cf5ff65fb391c470a670b052a4b"
     
     struct Auth {
@@ -23,10 +24,12 @@ class TMDBClient {
         static let apiKeyParam = "?api_key=\(TMDBClient.apiKey)"
         
         case getWatchlist
+        case getRequestToken
         
         var stringValue: String {
             switch self {
             case .getWatchlist: return Endpoints.base + "/account/\(Auth.accountId)/watchlist/movies" + Endpoints.apiKeyParam + "&session_id=\(Auth.sessionId)"
+            case .getRequestToken: return Endpoints.base + "/authentication/token/new" + Endpoints.apiKeyParam
             }
         }
         
